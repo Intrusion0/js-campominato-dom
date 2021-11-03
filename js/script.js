056d6389d7a04650cb347c50fbe3968ecbe0f562
Mario <lombardomario01@gmail.com>
2021-11-03 11:34:24 +0100

    Created files

---
+++ js/script.js
@@ -0,0 +1,97 @@
+// Griglia Campo Minato
+
+/* PRIMA PARTE
+
+L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro (o simili, l’importante è dare all’utente il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).
+*/
+
+/*  PRIMA PARTE
+
+--DONE  1. Genero la griglia di gioco con le varie celle in base alla difficoltà selezionata => for
+    --DONE  1.2 Se seleziona "Facile" (difficoltà 1), eseguirò un ciclo for che si ripeterà 100 volte, visto che dovrò stampare 100 celle. => for i < 100
+    --DONE  1.2 Se seleziona "Medio" (difficoltà 2), eseguirò un ciclo for che si ripeterà 81 volte, visto che dovrò stampare 81 celle. => for i < 81
+    --DONE  1.3 Se seleziona "Difficile" (difficoltà 3), eseguirò un ciclo for che si ripeterà 49 volte, visto che dovrò stampare 49 celle. => for i < 49
+--DONE  2. Creo un elemento => "div"
+--DONE  3. Aggiungo una classe all'elemento creato => classList.add("square")
+--DONE  4. Appendo l'elemento con la classe alla variabile che fa riferimento all'id di un tag html => .append 
+--DONE  5. Creo un evento al click della cella, in modo che si colora quando viene selezionata
+    --DONE  5.1 Creo una funzione => function squareActive()
+    --DONE  5.2 Aggiungo una classe all'elemento creato precedentemente ("div") => div.append("square-active")
+
+*/
+
+/* SECONDA PARTE
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su ogni cella:
+se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina,
+altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+(come detto sull’effetiva interfaccia fate voi, non ci son specifiche vincolanti, ma partite semplici)
+La partita termina quando il giocatore clicca su una bomba
+o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve scoprire tutte le bombe e comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato un quadratino con un numero consentito. (quindi se ci pensate dovrete tenere traccia del punteggio).
+*/
+
+/*  SECONDA PARTE
+
+1.
+
+*/
+
+const gridCont = document.getElementById("grid");
+const easyGame = document.getElementById("easy-btn");
+const mediumGame = document.getElementById("medium-btn");
+const difficultGame = document.getElementById("difficult-btn");
+const mySquare = document.querySelector(".square");
+
+easyGame.addEventListener("click", function() {
+    play(100, 'easy');
+});
+
+mediumGame.addEventListener("click", function() {
+    play(81, 'medium');
+});
+
+
+difficultGame.addEventListener("click", function() {
+    play(49, 'difficult');
+});
+
+
+// FUNZIONI
+
+// Genera le celle.
+function play(difficult, classes) {
+
+    if (document.querySelectorAll('.square').length) {
+        for (let j = 0; j < 100; j++) { // Da sistemare la condizione!!!!
+            document.querySelector('.square').remove();
+        }
+    }
+    
+    for (let i = 1; i <= difficult; i++) {
+        let squareCont = generateElement("div", "square", "square-" + classes);
+        squareCont.addEventListener("click", function() {
+                this.classList.add("square-active");
+                squareCont.innerText = i;
+                console.log("click");
+            }
+        );
+        gridCont.appendChild(squareCont);
+    }
+}
+
+// Permette di creare un elemento e aggiungere fino a 2 classi.
+const generateElement = (inputElement, inputClass, inputClassPlus) => {
+    let myCreateElement = document.createElement(inputElement); 
+    myCreateElement.classList.add(inputClass, inputClassPlus);
+    return myCreateElement
+}
+
+// Genera 16 numeri casuali univoci
